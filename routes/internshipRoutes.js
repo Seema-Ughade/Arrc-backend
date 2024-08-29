4c1626616ff99b7a29448d2d85e7a3e611218aa1
seema <seemarughade1363@gmail.com>
2024-08-29 11:55:07 +0530

    delet funtionality

--- routes/internshipRoutes.js
+++ routes/internshipRoutes.js
@@ -1,6 +1,28 @@
+// const express = require('express');
+// const multer = require('multer');
+// const { createApplication, getApplication, getAllApplications } = require('../controllers/internshipController');
+
+// const router = express.Router();
+// const upload = multer({ dest: 'uploads/' }); // Use multer to handle file uploads
+
+// // Route to create a new internship application
+// router.post('/internshipApplications', upload.single('cv'), createApplication);
+
+// // Route to get a specific internship application by ID
+// router.get('/internshipApplications/:id', getApplication);
+
+// // Route to get a all internship application by ID
+
+// router.get('/internshipApplications', getAllApplications);
+
+
+
+// module.exports = router;
+
+
 const express = require('express');
 const multer = require('multer');
-const { createApplication, getApplication, getAllApplications } = require('../controllers/internshipController');
+const { createApplication, getApplication, getAllApplications, deleteApplication } = require('../controllers/internshipController');
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' }); // Use multer to handle file uploads
@@ -11,10 +33,10 @@ router.post('/internshipApplications', upload.single('cv'), createApplication);
 // Route to get a specific internship application by ID
 router.get('/internshipApplications/:id', getApplication);
 
-// Route to get a all internship application by ID
-
+// Route to get all internship applications
 router.get('/internshipApplications', getAllApplications);
 
-
+// Route to delete a specific internship application by ID
+router.delete('/internshipApplications/:id', deleteApplication);
 
 module.exports = router;
